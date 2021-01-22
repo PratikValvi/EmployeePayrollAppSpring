254709cc001ab3b2ec2110d4a80875ddc53132c5
Pratik Valvi <pratikvalvi2305@gmail.com>
2021-01-22 15:22:02 +0530

    Created: Table from EmployeePayrollData Class in payroll_db

--- src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -3,15 +3,22 @@ package com.bridgelabz.employeepayrollapp.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.exception.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService {
 	
+	@Autowired
+	private EmployeePayrollRepository employeeRepository;
 	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 
 	@Override
@@ -29,9 +36,11 @@ public class EmployeePayrollService implements IEmployeePayrollService {
 
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData employeePayrollData = null;
-		employeePayrollData = new EmployeePayrollData(1, empPayrollDTO);
-		return employeePayrollData;
+		 EmployeePayrollData empData = null;
+	        empData = new EmployeePayrollData(empPayrollDTO);
+	        employeePayrollList.add(empData);
+	        log.debug("Emp Data: " +empData.toString());
+	        return employeeRepository.save(empData);
 	}
 
 	@Override
